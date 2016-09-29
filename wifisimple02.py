2fe9aa4088551e3dd5d7d0bf4dce0c875ef78df4
hw1989 <hw_hw1989@163.com>
2016-09-29 17:57:09 +0800

    Add files via upload

---
+++ wifisimple02.py
@@ -0,0 +1,103 @@
+
+#'a','e','beng','fo','mie','ga','nai','miu','ao','po','bo','yo','ou','ca','bie','bi','can',
+#'mo','o','luan','niao', 'nie','ti','ang', 'chi','ci','nang', 'nao', 'ne', 'nei', 'nen', 'neng',
+#'chu','chui','chuai','cou','cuo','cu','cuan', 'cui', 'cun','dang','de', 'deng', 'di','diao', 'die',
+#'ding', 'diu','en','er', 'fa','fou','gai','ge', 'gei','geng','gou','guai','ha','he', 'hei',
+#'hen','huai', 'huan','hun','huo','ji','jiong','jiu','juan','ka','kan','kao','keng','ken','kou',
+#'ku', 'kua', 'kuai','kuo', 'la','lai','lang', 'lao','leng','lou','lue','lun', 'luo','mai','man',
+#'mang', 'mao', 'me','men','mi', 'mian', 'niang','mou', 'mu','niu', 'nong', 'nu', 'nv', 'nuan',
+#'nue', 'nuo',   'pa', 'pai','pang', 'pao', 'pei','pen','pi', 'pian','pie', 'pin', 'ping','qia',
+#'qiang','qie','qu', 'quan', 'que','rang', 'rao','re', 'ren', 'reng', 'ri','ruan',
+#'sang','sao','shai','shao','she','sou','shou','shuan', 'shuo','suan','sui','suo','tiao','tie',
+#'tu','tui', 'tun', 'tuo','ya','ye','yong','yue','za', 'zai', 'zan', 'zang','zao','zei',
+#'zha','zhan','zhi','zhuai', 'zhuan', 'zhui','zong',  'zu', 'zuan', 'zui','shua','seng','qiong','liao',
+#'kuang','nin','bian','bang','ban','bu','cang','chong','duan','wa','sai','kui','dui','tai','se','sa',
+#'ran','lian','lie','chuo','dun','te','weng','zun',
+
+pinyin=['ai','an', 'ba', 'bai','bao', 'bei', 'ben', 
+         'biao',  'bin', 'bing', 'cai','cao', 'ce', 'ceng', 'cha', 'chai', 'chan',
+         'chang', 'chao', 'che', 'chen', 'cheng', 'chou','chuan', 'chuang',  'chun',  
+         'cong','da', 'dai','dan',  'dao',  'dian','dong', 'dou', 'du','duo',
+         'fan', 'fang', 'fei', 'fen', 'feng','fu','gan', 'gang', 'gao',  'gen',
+          'gong',  'gu', 'gua',  'guan', 'guang', 'gui', 'gun', 'guo','hai', 'han', 'hang', 'hao', 
+        'heng', 'hong', 'hou', 'hu', 'hua',  'huang','hui','jia', 'jian', 'jiang', 'jiao', 'jie',
+         'jin', 'jing',   'ju',  'jue', 'jun','kai',  'kang',  'ke','kong', 
+          'kuan',   'kun','lan',  'le', 'lei',  'li', 'lia',
+           'liang',   'lin', 'ling', 'liu', 'long',
+           'lu', 'lv','ma','mei','meng','miao','min', 'ming','na',  'nan',
+           'ni', 'nian', 'ning','pan','peng','piao','pu', 'qi',  'qian',  'qiao',  'qin', 'qing',
+             'qiu',  'qun',  'rong', 'rou', 'ru',  'rui',
+            'run', 'ruo', 'san', 'sen',
+             'sha',  'shan', 'shang',   'shen',
+            'sheng', 'shi',  'shu',  'shuai',  'shuang',
+            'shui', 'shun', 'si', 'song',  'su',  
+            'sun',  'ta',  'tan', 'tang', 'tao',  'teng',
+             'tian', 'ting', 'tong', 'tou',  'tuan',
+              'wai', 'wan', 'wang', 'wei', 'wen',
+             'wo', 'wu', 'xi', 'xia', 'xian', 'xiang', 'xiao', 'xie',
+            'xin', 'xing', 'xiong', 'xiu', 'xu', 'xuan', 'xue', 'xun', 
+            'yan', 'yang', 'yao',  'yi', 'yin', 'ying',  
+            'you', 'yu', 'yuan',  'yun', 
+             'ze',  'zen', 'zeng',  'zhai',  'zhang',
+            'zhao', 'zhe', 'zhen', 'zheng',  'zhong', 'zhou', 'zhu',
+            'zhua',  'zhuang', 'zhun', 'zhuo', 'zi','zou',
+              'zuo']
+net=['sb','2b','abc','qwert']
+space=['',]
+english=['and','fuck','love','who','no','u','you','your','hello','hi','dream','kiss','is','i','admin']
+number=['00','000','0000','00000','000000','1314','120','110','119','123','12345','123456','123456789','12345678',
+        '111','1111','11111','111111','250','251','210','222','2222','555','666','666666','88','888','8888','88888',
+        '888888','88888888','999','99','999','9999','']
+#date=['1987','1988','1989','1990','1991','1992','1993','1994','2016','01','03','04','05','06','07','08','09','10','11','12','1','2','3','4','5'
+#,'6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+
+#14
+year1=['1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993']
+#23
+month1=['01','02','03','04','05','06','07','08','09','10','11','12']
+
+month2=['1','2','3','4','5','6','7','8','9','10','11','12']
+#42
+day1=['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+
+day2=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+
+#print(len(pinyin)+len(net)+len(english)+len(number)+len(date))
+print('pinyin  %d' % len(pinyin))
+print('net  %d' % len(net))
+print('english  %d' % len(english))
+print('number  %d' % len(number))
+print('year  %d' % len(year1))
+print('month1  %d' % len(month1))
+print('day1  %d' % len(day1))
+print('month2  %d' % len(month2))
+print('day2  %d' % len(day2))
+#  199*200*200*(37+14*12*31+14*12*31)=199*200*200*10453=83205880000
+#3个字符组+3个日期
+count=0
+for str1 in (pinyin):
+    continue
+    for str2 in (pinyin+space):
+        for str3 in (pinyin+space):
+            if str1==str2 and str2==str3 :
+                continue
+            #数字
+            for str4 in (number):
+                if len(str1+str2+str3+str4)>7 and len(str1+str2+str3+str4)<13:
+                    count+=1
+            #日期
+            for str4 in (year1):
+                for str5 in month2:
+                    for str6 in day2:
+                       if len(str1+str2+str3+str4+str5+str6)>7 and len(str1+str2+str3+str4+str5+str6)<13:
+                            count+=1 
+                for str5 in (month1):
+                    for str6 in (day1):
+                        if len(str1+str2+str3+str4+str5+str6)>7 and len(str1+str2+str3+str4+str5+str6)<13:
+                            count+=1
+            #年
+            for str4 in (year1):
+                if len(str1+str2+str3+str4)>7 and len(str1+str2+str3+str4)<13:
+                    count+=1
+
+
